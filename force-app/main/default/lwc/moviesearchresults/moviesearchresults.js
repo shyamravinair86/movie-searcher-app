75da4db74798bd3926e22e8dd276bdd674b655d7
Shyam Nair <shyam.ravinair@gmail.com>
2019-10-21 20:48:03 +0530

    pushing changes made on moviesearchresults and apex class

--- force-app/main/default/lwc/moviesearchresults/moviesearchresults.js
+++ force-app/main/default/lwc/moviesearchresults/moviesearchresults.js
@@ -1,5 +1,35 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+import getMovies from '@salesforce/apex/MovieSearcherController.searchMovies';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Moviesearchresults extends LightningElement {
     @api movieSearchText;
+    @track movies;
+
+    @wire(getMovies, {searchTerm: '$movieSearchText'}) 
+    movies({data, error}) {
+        if(data) {
+            this.movies = data;
+        }
+        else if(error) {
+            this.showToast('Cars Not Found', error.body.message, 'error');
+        }
+    }
+
+    showToast(title, message, variant){
+        const evt = new ShowToastEvent({
+            title : title,
+            message : message,
+            variant : variant,
+        });
+        this.dispatchEvent(evt);
+    }
+
+    get moviesFound() {
+        if(this.movies) {
+            return true;
+        }
+        return false;
+    }
+    
 }
